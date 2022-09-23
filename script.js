f57f361875e97a1d94d110d235276e0955fa8779
rachael-pluta <109520528+rachael-pluta@users.noreply.github.com>
2022-09-23 17:26:14 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,62 @@
+var currentDateEl = $("#date-and-time");
+var currentDate;
+
+var movieTitleInput = document.querySelector("#search-input");
+var searchButton = document.querySelector("#search-button");
+var movieList = document.querySelector("#list-history");
+var movieTitleText = movieTitleInput.value.trim();
+
+var movieTitles = [];
+
+function init() {
+    var storedMovies = JSON.parse(localStorage.getItem("movie-titles"));
+
+    if (storedMovies !== null) {
+        movieTitles = storedMovies;
+    }
+
+    renderMovieTitles();
+    currentMomentDate();
+};
+
+// Determines the current time
+function currentMomentDate() {
+    currentDate = dayjs().format("dddd hh:mm A");
+    currentDateEl.text(currentDate);
+};
+
+// Time refreshes on a set Interval
+var refresh = setInterval(function () {
+    currentMomentDate();
+}, 1000);
+
+function renderMovieTitles() {
+    for (var i = 0; i < movieTitles.length; i++) {
+        var movieTitle = movieTitles[i];
+
+        var li = document.createElement("li");
+        li.textContent = movieTitle;
+        li.setAttribute("data-index", i);
+        movieList.appendChild(li);
+    }
+};
+
+function storeMovieTitles() {
+    localStorage.setItem("movie-titles", JSON.stringify(movieTitles));
+};
+
+searchButton.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    if (movieTitleText === "") {
+        return;
+    }
+
+    movieTitles.push(movieTitleText);
+    movieTitleInput.value = "";
+
+    storeMovieTitles();
+    renderMovieTitles();
+});
+
+init()
